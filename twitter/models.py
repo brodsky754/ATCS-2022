848abb98b21a7e66e5af5735a471c326b934c3fb
brodsky754 <nate.goldberg@gmail.com>
2023-03-30 21:29:29 -0700

    finished models.py

--- twitter/models.py
+++ twitter/models.py
@@ -25,6 +25,13 @@ class User(Base):
                              primaryjoin="User.username==Follower.following_id",
                              secondaryjoin="User.username==Follower.follower_id",
                              overlaps="following")
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+    def __repr__(self):
+        return "@" + self.username
 
 
 class Follower(Base):
@@ -35,26 +42,57 @@ class Follower(Base):
     follower_id = Column('follower_id', TEXT, ForeignKey('users.username'))
     following_id = Column('following_id', TEXT, ForeignKey('users.username'))
 
-class Tweet(Base):
-    __tablename__ = "tweet"
-    # TODO: Complete the class
-    id = Column("id", INTEGER, primary_key=True)
-    content = Column('content',TEXT)
-    timestamp = Column('timestamp', DATETIME)
-    username = Column('username', TEXT, ForeignKey('users.username'))
-    pass
+    def __init__(self, follower_id, following_id):
+        self.follower_id = follower_id
+        self.following_id = following_id
 
-class Tag(Base):
-    __tablename__ = "tag"
+class Tweet(Base):
     # TODO: Complete the class
-    id = Column("id", INTEGER, primary_key=True)
-    content = Column('content',TEXT)
-    pass
+    __tablename__ = "tweets"
+
+    id = Column("id", INTEGER, primary_key = True)
+    content = Column("content", TEXT)
+    timestamp = Column("timestamp", TEXT)
+    username = Column("username", TEXT)
+    tags = relationship("Tag", secondary = "tweet_tags", back_populates = "tweets")
+    
+    
+
+    def __init__(self, content, timestamp, username):
+        self.content = content
+        self.timestamp = timestamp
+        self.username = username
+
+    def __repr__(self):
+        list_tags = ""
+        for t in self.tags:
+            list_tags = list_tags + t.content
+        return "@" + self.username + "\n" + self.content  + "\n" + list_tags + "\n" + self.timestamp 
+        
+class Tag(Base):
+    # TODO: Complete the class
+    __tablename__ = "tags"
+
+    id = Column("id", INTEGER, primary_key = True)
+    content = Column("content", TEXT, nullable = False)
+    tweets = relationship("Tweet", secondary = "tweet_tags", back_populates = "tags")
+
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return "#" + self.content
+    
 
 class TweetTag(Base):
-    __tablename__ = "tweettag"
     # TODO: Complete the class
-    id = Column("id", INTEGER, primary_key=True)
-    tweet_id = Column('tweet_id',TEXT, ForeignKey('tweets.id'))
-    tag_id = Column('tag_id',TEXT, ForeignKey('tags.id'))
-    pass
+    __tablename__ = "tweet_tags"
+
+    id = Column("id", INTEGER, primary_key = True)
+    tweet_id = Column("tweet_id", INTEGER, ForeignKey('tweets.id'))
+    tag_id = Column("tag_id", INTEGER, ForeignKey('tags.id'))
+
+    def __init__(self, tweet_id, tag_id):
+        self.tweet_id = tweet_id
+        self.tag_id = tag_id
+
